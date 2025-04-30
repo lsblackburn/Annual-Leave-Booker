7f5a6394df68582c562ed36e5fe5a25007122add
lsblackburn <lewisblackburn9@gmail.com>
2025-04-30 22:12:00 +0100

    Javascript to toggle visibility of password

---
+++ static/javascript.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $('#passwordVisible img:nth-child(2)').hide();
+
+    $('#passwordVisible img').on('click', function () {
+
+        $('#passwordVisible img:nth-child(2)').toggle();
+        $('#passwordVisible img:nth-child(1)').toggle();
+
+        // Toggle input type between password and text
+        var passwordInput = $('#passwordContainer input');
+        var currentType = passwordInput.attr('type');
+
+        if (currentType === 'password') {
+            passwordInput.attr('type', 'text');
+        } else {
+            passwordInput.attr('type', 'password');
+        }
+
+    });
+
+});
