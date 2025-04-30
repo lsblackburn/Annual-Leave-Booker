21a84a43216d43f4e417bf4f63372bb96cfe7465
lsblackburn <lewisblackburn9@gmail.com>
2025-04-30 16:06:38 +0100

    Updated gitignore so that instance folder with database isn't uploaded

--- app.py
+++ app.py
@@ -4,9 +4,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+# Create a database model for the user
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+    
+    def __repr__(self) -> str:
+        return f'<User {self.name}>'
+
+
+
+# Routes to webpages
 @app.route('/')
 def index():
     return render_template('pages/dashboard.html')
 
 if __name__ == '__main__':
+    
+    with app.app_context():
+        db.create_all()  # Create the database tables if they don't exist
+    
     app.run(debug=True)
