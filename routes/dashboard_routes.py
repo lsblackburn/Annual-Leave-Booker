0a5dea73f74462062211df2ddf6302d73e2a0bbe
lsblackburn <lewisblackburn9@gmail.com>
2025-06-11 23:51:40 +0100

    Updated dashboard to display upcomong annual leave within the next 4 weeks

--- routes/dashboard_routes.py
+++ routes/dashboard_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, g
+from datetime import datetime, timedelta
 from models import AnnualLeave
 from decorators import login_required
 
@@ -8,8 +9,13 @@ dashboard = Blueprint('dashboard', __name__)
 @dashboard.route('/')
 @login_required
 def dashboard_view():
+    
+    # Get current datetime and datetime 4 weeks ahead
+    now = datetime.now()
+    four_weeks_later = now + timedelta(weeks=4)
+    
     # Fetch all approved leave records, ordered by updated_at descending
-    leaves = AnnualLeave.query.where(AnnualLeave.status == 'Approved').order_by(AnnualLeave.updated_at.desc()).all()
+    leaves = AnnualLeave.query.filter(AnnualLeave.status == 'Approved', AnnualLeave.start_date >= now, AnnualLeave.start_date <= four_weeks_later).order_by(AnnualLeave.start_date.asc()).all()
 
     # Render the dashboard template with user role and leave data
     return render_template(
@@ -22,7 +28,6 @@ def dashboard_view():
 @dashboard.route('/your-leaves')
 @login_required
 def your_leave_view():
-
     # Fetch all leave records for the current user, ordered by updated_at descending. If rejected do not display on the dashboard
     leaves = AnnualLeave.query.filter_by(user_id=g.user.id).order_by(AnnualLeave.updated_at.desc()).all()
 
