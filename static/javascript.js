c9f26e5181dafd4ed3238bab6b06ee526a4b1b13
lsblackburn <lewisblackburn9@gmail.com>
2025-07-11 05:03:37 +0100

    Removed redundant confirmation_pop function

--- static/javascript.js
+++ static/javascript.js
@@ -22,48 +22,8 @@ $(document).ready(function () {
 });
 
 $(document).ready(function() {
-
     $('input[type="date"]').on('click', function() {
-
         this.showPicker && this.showPicker();
         // When clicking on the whole input, show the native date picker
-
-    });
-
-});
-
-
-$(document).ready(function () {
-    $('.confirmation_pop').on('click', function (e) {
-        e.preventDefault();
-        var form = $(this).closest('form');
-        
-        if ($(this).hasClass('delete-user-form')) {
-            var title = 'Are you sure want to delete this user?';
-            var text = "You won't be able to revert this!";
-            var confirmButtonText = 'Yes, delete it!';
-        } else if ($(this).hasClass('revoke-user-form')) {
-            var title = 'Are you sure want to revoke admin rights from this user?';
-            var text = "This is a reversible action!";
-            var confirmButtonText = 'Yes, revoke it!';
-        } else if ($(this).hasClass('promote-user-form')) {
-            var title = 'Are you sure want to promote this user to admin?'; 
-            var text = "This is a reversible action!";
-            var confirmButtonText = 'Yes, promote it!';
-        }
-        
-        Swal.fire({
-            title: title,
-            text: text,
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: confirmButtonText
-        }).then(function (result) {
-            if (result.isConfirmed) {
-                form.submit();
-            }
-        });
     });
 });
